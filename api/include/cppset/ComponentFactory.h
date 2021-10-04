6f0999a12791d9e122da1cbad63cc38b674c3f9e
sofiaf20 <69475001+sofiaf20@users.noreply.github.com>
2021-10-04 18:22:26 -0300

    Update ComponentFactory.h

--- api/include/cppset/ComponentFactory.h
+++ api/include/cppset/ComponentFactory.h
@@ -24,42 +24,42 @@ class ComponentFactory
     public:
         ComponentFactory()
         { 
-            //THE LOADER:
+            //THE LOADER: // carga el puntero inteligente sobre la libreria
             this->loader = std::make_shared<LibraryLoader>();
-            //INIREADER:
+            //INIREADER: // inicializa un nuevo reader
             std::shared_ptr<IniReader> iniReader( new IniReader() );
-            iniReader->open("configuration.ini");
-            this->componentsPath = iniReader->selectSection("GENERAL")->getValue("componentsPath");
+            iniReader->open("configuration.ini"); // lo abre
+            this->componentsPath = iniReader->selectSection("GENERAL")->getValue("componentsPath"); // vincula el path
         }
-        virtual ~ComponentFactory()
+        virtual ~ComponentFactory() //libera la libreria el destructor
         {
             this->loader->freeLibrary();
         }
         
-        template<typename InterfaceType> std::shared_ptr<InterfaceType> create(std::string componentName)
+        template<typename InterfaceType> std::shared_ptr<InterfaceType> create(std::string componentName) // crea el nombre del componente
         {
-            //LOAD:
+            //LOAD: //carga el path mas el nombre del componente en un puntero
             void* load = this->loader->loadLibrary(this->componentsPath + componentName);
 
             //NULL COMPONENT OBJECT:
             std::shared_ptr<InterfaceType> componentObject = nullptr;
-            if(load)
+            if(load) // su esta cargando..
             {
-                //CREATE:
+                //CREATE: llama a una funcion externa de crear y el puntero lo vincula con un string
                 typedef std::shared_ptr<InterfaceType> ( *Factory ) (std::string);
                 Factory factory = ( Factory ) this->loader->getExternalFunction( "create" );
 
                 if(factory)
                 {
-
+                    //dice si el componente creado esta igualado con el nombre id  
                     std::shared_ptr<InterfaceType> createdComponent = factory(typeid(InterfaceType).name());//construyo una sola vez
                     if( std::dynamic_pointer_cast<InterfaceType> (createdComponent) != nullptr )
-                    {
+                    {   // si  el componente no esta vacio, se iguala el objeto con el componente creado
                         componentObject = createdComponent;
                     }
                     else
                     {
-                        this->loader->freeLibrary();
+                        this->loader->freeLibrary(); // si no cumple la condicion muestra mensaje de error
                         std::cout << "Error: The component doesn't implement the interface: " << typeid(InterfaceType).name() << std::endl;
                         exit(-1);
                     }
@@ -78,7 +78,7 @@ class ComponentFactory
                 std::cout << "Error: Failed to load the component: " << componentName << std::endl;
                 exit(-1);
             }
-            return componentObject;
+            return componentObject; // retorna el objeto creado
         }
 };
 #endif // COPONENT_FACTORY_H
